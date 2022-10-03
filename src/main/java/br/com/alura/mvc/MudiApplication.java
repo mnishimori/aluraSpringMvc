71d4f14a0848512036ad5924ac93ca409f080e43
mnishimori <mnishimori@yahoo.com.br>
2022-10-03 08:58:40 -0300

    SDJPA

--- src/main/java/br/com/alura/mvc/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/MudiApplication.java
@@ -2,12 +2,40 @@ package br.com.alura.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Example;
+
+import br.com.alura.mvc.model.Pedido;
+import br.com.alura.mvc.repository.PedidoRepository;
 
 @SpringBootApplication
 public class MudiApplication {
+	
+	private final PedidoRepository pedidoRepository;
+	
+
+	public MudiApplication(PedidoRepository pedidoRepository) {
+		super();
+		this.pedidoRepository = pedidoRepository;
+		
+		this.popularPedidos();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MudiApplication.class, args);
+	}
+
+
+	private void popularPedidos() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Fire TV Stick 4k");
+		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/G/32/apparel/rcxgs/tile._CB483369971_.gif");
+		pedido.setUrlProduto("https://www.amazon.com.br/Fire-TV-Stick-4K-Streaming/dp/B0872Y93TY?ref=dlx_deals_gd_dcl_img_1_b92ea1f3_dt_sl15_43");
+		pedido.setDescricao("Fire TV Stick 4K com Controle Remoto por Voz com Alexa (inclui comandos de TV) | Dolby Vision");
+		
+		if (!this.pedidoRepository.findOne(Example.of(pedido)).isPresent()) {
+			pedidoRepository.save(pedido);
+		}
 	}
 
 }
--- src/main/java/br/com/alura/mvc/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/controller/HomeController.java
@@ -1,31 +1,40 @@
 package br.com.alura.mvc.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.mvc.model.Pedido;
+import br.com.alura.mvc.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/pedidos")
 public class HomeController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
-	public String home(Model model) {
+	public ModelAndView home(Model model) {
 		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Fire TV Stick 4k");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/G/32/apparel/rcxgs/tile._CB483369971_.gif");
-		pedido.setUrlProduto("https://www.amazon.com.br/Fire-TV-Stick-4K-Streaming/dp/B0872Y93TY?ref=dlx_deals_gd_dcl_img_1_b92ea1f3_dt_sl15_43");
-		pedido.setDescricao("Fire TV Stick 4K com Controle Remoto por Voz com Alexa (inclui comandos de TV) | Dolby Vision");
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("pedidos", pedidos);
 		
-		model.addAttribute("pedidos", pedidos);
-		
-		return "home";
+		return mv;
 	}
 
+	@GetMapping()
+	public List<Pedido> lista() {
+		
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		
+		return pedidos;
+	}
 }
