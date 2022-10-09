449747a28a000d97adcd8a878bf0bc62c1d2f029
mnishimori <mnishimori@yahoo.com.br>
2022-10-09 18:17:26 -0300

    Form de login e logout

--- src/main/java/br/com/alura/mvc/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/MudiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.domain.Example;
 
 import br.com.alura.mvc.model.Pedido;
+import br.com.alura.mvc.model.StatusPedido;
 import br.com.alura.mvc.repository.PedidoRepository;
 
 @SpringBootApplication
@@ -17,7 +18,7 @@ public class MudiApplication {
 		super();
 		this.pedidoRepository = pedidoRepository;
 		
-		this.popularPedidos();
+		// this.popularPedidos();
 	}
 
 
@@ -32,6 +33,7 @@ public class MudiApplication {
 		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/G/32/apparel/rcxgs/tile._CB483369971_.gif");
 		pedido.setUrlProduto("https://www.amazon.com.br/Fire-TV-Stick-4K-Streaming/dp/B0872Y93TY?ref=dlx_deals_gd_dcl_img_1_b92ea1f3_dt_sl15_43");
 		pedido.setDescricao("Fire TV Stick 4K com Controle Remoto por Voz com Alexa (inclui comandos de TV) | Dolby Vision");
+		pedido.setStatus(StatusPedido.AGUARDANDO);
 		
 		if (!this.pedidoRepository.findOne(Example.of(pedido)).isPresent()) {
 			pedidoRepository.save(pedido);
--- src/main/java/br/com/alura/mvc/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/alura/mvc/dto/RequisicaoNovoPedido.java
@@ -3,6 +3,7 @@ package br.com.alura.mvc.dto;
 import javax.validation.constraints.NotBlank;
 
 import br.com.alura.mvc.model.Pedido;
+import br.com.alura.mvc.model.StatusPedido;
 
 public class RequisicaoNovoPedido {
 
@@ -48,6 +49,7 @@ public class RequisicaoNovoPedido {
 		pedido.setNomeProduto(nomeProduto);
 		pedido.setUrlImagem(urlImagem);
 		pedido.setUrlProduto(urlProduto);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
 		return pedido;
 	}
 	
